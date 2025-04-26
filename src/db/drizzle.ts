7dfcfc6782926d20886a9b3ef33eeb049b3a5aa5
ErickLungazo <ericlungazo@gmail.com>
2025-04-26 18:29:37 +0300

    directly

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,6 +1,7 @@
-import { config } from "dotenv";
-import { drizzle } from 'drizzle-orm/neon-http';
+import { drizzle } from "drizzle-orm/neon-http";
 
-config({ path: ".env" }); // or .env.local
+// config({ path: ".env" }); // or .env.local
+const DATABASE_URL =
+  "postgresql://neondb_owner:npg_NtK9wWsFm5CQ@ep-cool-cell-a4h9k16t-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+export const db = drizzle(DATABASE_URL);
